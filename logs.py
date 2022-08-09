b6c1b4e8f1c3be6dc9d38051ca422e48a1c94b4d
ShaharBrandman <67471700+ShaharBrandman@users.noreply.github.com>
2022-08-10 01:48:27 +0300

    Add files via upload

---
+++ logs.py
@@ -0,0 +1,50 @@
+import json
+
+"""a = "BTC": [],
+        "ETH": [],
+        "ADA": [],
+        "SOL": [],
+        "DOGE": []"""
+
+LEVERAGE = 35
+MAX_POSITIONS = 3
+
+currentTotal = 100
+budget = currentTotal / MAX_POSITIONS
+
+print(f'Starting backtesting of pumpy.py with {LEVERAGE}X Leverage, portoflio: {currentTotal}, budget: {budget}, maxPositions: {MAX_POSITIONS}')
+
+with open('logs.json', 'r') as r:
+    data = json.loads(r.read())
+    
+    pTotal = currentTotal
+
+    #days
+    for i in data:
+        currentTotal = 0
+
+        #currencies
+        for j in data[i]:
+            #P&L
+            currencyPL = 0
+            for k in data[i][j]:
+                pl = float(k) * LEVERAGE
+                currencyPL = currencyPL + pl
+            
+            aftermath = ((100 + currencyPL) * budget) / 100
+            currentTotal = currentTotal + aftermath
+            #print(f'{i} {j} P&L: {currencyPL}% with {LEVERAGE}X LEVERAGE P&L: {currencyPL * LEVERAGE}%, startingAmount: {budget}, current: {aftermath}')
+
+        gain = (currentTotal * 100) / pTotal
+
+        if gain > 100:
+            gain = gain - 100
+        else:
+            gain = 100 - gain
+
+        print(f'{i} currentTotal: {currentTotal}, P&L: {gain}%, previousTotal: {pTotal}')
+        pTotal = currentTotal
+
+
+            
+        

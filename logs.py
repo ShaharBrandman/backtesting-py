605e93b19d59af96734b43c16b4892c2dfe9f0af
ShaharBrandman <67471700+ShaharBrandman@users.noreply.github.com>
2022-08-10 09:04:02 +0300

    Add files via upload

--- logs.py
+++ logs.py
@@ -7,41 +7,49 @@ import json
         "DOGE": []"""
 
 LEVERAGE = 35
-MAX_POSITIONS = 4
+MAX_POSITIONS = 3
 
-currentTotal = 100
-budget = currentTotal / MAX_POSITIONS
+totalDayTotal = 100
+budget = totalDayTotal / MAX_POSITIONS
 
-print(f'Starting backtesting with {LEVERAGE}X Leverage, portoflio: {currentTotal}, budget: {budget}, maxPositions: {MAX_POSITIONS}')
+print(f'Starting backtesting with {LEVERAGE}X Leverage, portoflio: {totalDayTotal}, budget: {budget}, maxPositions: {MAX_POSITIONS}')
 
 with open('logs.json', 'r') as r:
     data = json.loads(r.read())
     
-    pTotal = currentTotal
+    pTotal = totalDayTotal
 
     #days
     for i in data:
-        currentTotal = 0
-
+        counter = 0
+        totalDayTotal = 0
         #currencies
         for j in data[i]:
+            if counter == MAX_POSITIONS:
+                break
+            else:
+                counter = counter + 1
+            
             #P&L
-            currencyPL = 0
+            totalCurrencyPL = 0
             for k in data[i][j]:
                 pl = float(k) * LEVERAGE
-                currencyPL = currencyPL + pl
+                totalCurrencyPL = totalCurrencyPL + pl
             
-            aftermath = ((100 + currencyPL) * budget) / 100
-            currentTotal = currentTotal + aftermath
-            #print(f'{i} {j} P&L: {currencyPL}% with {LEVERAGE}X LEVERAGE P&L: {currencyPL * LEVERAGE}%, startingAmount: {budget}, current: {aftermath}')
+            #budget + P&L
+            aftermath = ((100 + totalCurrencyPL) * budget) / 100
+            
+            #total of the whole day
+            totalDayTotal = totalDayTotal + aftermath
+            print(f'{i} {j} P&L: {totalCurrencyPL}%, startingAmount: {budget}, current: {aftermath}')
 
-        gain = (currentTotal * 100) / pTotal
+        gain = (totalDayTotal * 100) / pTotal
 
         #idk man, makes no sense
         gain = gain - 100
 
-        print(f'{i} currentTotal: {currentTotal}, P&L: {gain}%, previousTotal: {pTotal}')
-        pTotal = currentTotal
+        print(f'{i} totalDayTotal: {totalDayTotal}, P&L: {gain}%, previousTotal: {pTotal}')
+        pTotal = totalDayTotal
 
 
             

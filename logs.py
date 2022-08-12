6758f77f6740bbd0550c3a60ec28e659e4fac2ae
ShaharBrandman <shaharbrandman@gmail.com>
2022-08-12 16:55:55 -0700

    modified:   logs.py

--- logs.py
+++ logs.py
@@ -7,8 +7,8 @@ STARTING_PORTOFLIO = 100
 print(f'Starting backtesting with {LEVERAGE}X Leverage, portoflio: {STARTING_PORTOFLIO}, budget: {STARTING_PORTOFLIO / MAX_POSITIONS}, maxPositions: {MAX_POSITIONS}')
 print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
 
-def dailyBudgetStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO):
-    print('Running daily budget strat (100% Portfolio)')
+def dailyPortfolioBudgetStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO):
+    print('Running daily portfolio budget strat (100% Portfolio)')
     
     totalDayGainInMoney = STARTING_PORTOFLIO
     budget = totalDayGainInMoney / MAX_POSITIONS
@@ -130,7 +130,7 @@ def onSpotBudgetStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO):
             dailyGainPercentage = dailyGainPercentage - 100
             totalGainPercentage = totalGainPercentage - 100
 
-            print(f'{i} totalDayGainInMoney: {totalDayGainInMoney}, daily P&L: {dailyGainPercentage}%, actual P&L: {totalGainPercentage}%, previousPortofolio: {previousPortofolio}')
+            print(f'{i} totalDayGainInMoney: {totalDayGainInMoney}, daily P&L: {dailyGainPercentage}%, actual P&L: {totalGainPercentage}%, previousBudget: {previousPortofolio}')
             previousPortofolio = totalDayGainInMoney
 
         accuracy = (wins * 100) / (wins + loss)
@@ -190,7 +190,7 @@ def coinTraderOnSpotBudgetStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO):
             dailyGainPercentage = dailyGainPercentage - 100
             totalGainPercentage = totalGainPercentage - 100
 
-            print(f'{i} totalDayGainInMoney: {totalDayGainInMoney}, daily P&L: {dailyGainPercentage}%, actual P&L: {totalGainPercentage}%, previousPortofolio: {previousPortofolio}')
+            print(f'{i} totalDayGainInMoney: {totalDayGainInMoney}, daily P&L: {dailyGainPercentage}%, actual P&L: {totalGainPercentage}%, previousBudget: {previousPortofolio}')
             previousPortofolio = totalDayGainInMoney
 
         accuracy = (wins * 100) / (wins + loss)
@@ -198,8 +198,8 @@ def coinTraderOnSpotBudgetStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO):
         print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
         print(f'accuracy of: {accuracy}% with {wins} wins and {loss} losses')
 
-def coinTraderDailyBudgetStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO):
-    print('Running coinTrader daily budget strat (50% Portfolio)')
+def coinTraderDailyPortfolioBudgetStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO):
+    print('Running coinTrader daily portfolio budget strat (50% Portfolio)')
     
     totalDayGainInMoney = STARTING_PORTOFLIO
     budget = (totalDayGainInMoney / 2) / MAX_POSITIONS
@@ -269,11 +269,75 @@ def coinTraderDailyBudgetStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO):
         print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
         print(f'accuracy of: {accuracy}% with {wins} wins and {loss} losses but with Profit chance of: {profitChance}%')
 
+def onSpotBudgetPercentagePortfolioStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO, PERCENTAGE = 100):
+    print(f'Running on spot budget strat ({PERCENTAGE}% Portfolio, no change)')
+
+    totalDayGainInMoney = (STARTING_PORTOFLIO * PERCENTAGE) / 100
+    budget = totalDayGainInMoney / MAX_POSITIONS
+    with open('logs.json', 'r') as r:
+        data = json.loads(r.read())
+        
+        previousPortofolio = totalDayGainInMoney
+        wins = 0
+        loss = 0
+
+        #days
+        for i in data:
+            counter = 0
+            totalDayGainInMoney = 0
+
+            #currencies
+            for j in data[i]:
+                #limit the amount of positions active at the same time in the same day
+                if counter == MAX_POSITIONS:
+                    break
+                else:
+                    counter = counter + 1
+                
+                #P&L
+                aftermath = budget
+                for k in data[i][j]:
+                    pl = float(k) * LEVERAGE
+                    
+                    if pl > 0:
+                        wins = wins + 1
+                    else: 
+                        loss = loss + 1
+
+                    pAftermath = aftermath
+
+                    #budget + P&L
+                    aftermath = ((100 + pl) * aftermath) / 100
+                    #print(f'{i} {j} new position P&L: {pl}%, startingAmount: {pAftermath}, current: {aftermath}')
+                
+                #total of the whole day
+                totalDayGainInMoney = totalDayGainInMoney + aftermath
+
+            budget = totalDayGainInMoney / MAX_POSITIONS
+            totalGainPercentage = (totalDayGainInMoney * 100) / STARTING_PORTOFLIO
+            dailyGainPercentage = (totalDayGainInMoney * 100) / previousPortofolio
+
+            #idk man, makes no sense
+            dailyGainPercentage = dailyGainPercentage - 100
+            totalGainPercentage = totalGainPercentage - 100
+
+            print(f'{i} totalDayGainInMoney: {totalDayGainInMoney}, daily P&L: {dailyGainPercentage}%, actual P&L: {totalGainPercentage}%, previousBudget: {previousPortofolio}')
+            previousPortofolio = totalDayGainInMoney
+
+        accuracy = (wins * 100) / (wins + loss)
+        
+        print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+        print(f'accuracy of: {accuracy}% with {wins} wins and {loss} losses')
+
 if __name__ == '__main__':
+    onSpotBudgetPercentagePortfolioStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO, 50)
+    print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
     onSpotBudgetStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO)
     print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+    '''
     coinTraderOnSpotBudgetStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO)
     print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
-    dailyBudgetStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO)
+    dailyPortfolioBudgetStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO)
     print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
-    coinTraderDailyBudgetStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO)
+    coinTraderDailyPortfolioBudgetStrat(LEVERAGE, MAX_POSITIONS, STARTING_PORTOFLIO)
+    '''
